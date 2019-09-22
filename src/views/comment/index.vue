d9159df3b35843f154cd7842bd4d2810e1647cad
unknown <819563056@qq.com>
2019-09-22 17:42:30 +0800

    黑马头条PC-评论列表-打开和关闭评论

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -8,10 +8,17 @@
         </bread-crumb>
         <el-table :data='list'>
             <el-table-column width="600px"  label='标题' prop='title'></el-table-column>
-            <el-table-column align="center" label='评论状态' prop='comment_status'></el-table-column>
+            <el-table-column :formatter='formatter'  align="center" label='评论状态' prop='comment_status'></el-table-column>
             <el-table-column align="center" label='总评论数' prop='total_comment_count'></el-table-column>
             <el-table-column align="center" label='粉丝评论数' prop='fans_comment_count'></el-table-column>
-            <el-table-column align="center" label='操作'></el-table-column>
+            <el-table-column align="center" label='操作'>
+                <template slot-scope="obj">
+                <el-button size='small' type="text">修改</el-button>
+                <el-button size='small' type="text">{{
+                    obj.row.comment_status ? '关闭评论' : '打开评论'
+                    }}</el-button>
+            </template>
+            </el-table-column>
         </el-table>
     </el-card>
 </template>
@@ -32,6 +39,9 @@ export default {
       }).then(result => {
         this.list = result.data.results
       })
+    },
+    formatter (row, column, cellValue, index) {
+      return cellValue ? '正常' : '关闭'
     }
 
   },
