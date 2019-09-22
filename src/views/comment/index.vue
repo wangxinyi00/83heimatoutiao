14bd00ee6e045904d43171c068bb0c2c9703ec1a
unknown <819563056@qq.com>
2019-09-22 17:12:32 +0800

    黑马头条PC-评论列表-评论数据加载到页面及状态过滤

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -6,12 +6,38 @@
                 评论列表
             </template>
         </bread-crumb>
+        <el-table :data='list'>
+            <el-table-column width="600px"  label='标题' prop='title'></el-table-column>
+            <el-table-column align="center" label='评论状态' prop='comment_status'></el-table-column>
+            <el-table-column align="center" label='总评论数' prop='total_comment_count'></el-table-column>
+            <el-table-column align="center" label='粉丝评论数' prop='fans_comment_count'></el-table-column>
+            <el-table-column align="center" label='操作'></el-table-column>
+        </el-table>
     </el-card>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      list: []
+    }
+  },
+  methods: {
+    getComment () {
+      this.$axios({
+        url: '/articles',
+        method: '',
+        params: { response_type: 'comment' }
+      }).then(result => {
+        this.list = result.data.results
+      })
+    }
 
+  },
+  created () {
+    this.getComment()
+  }
 }
 </script>
 
