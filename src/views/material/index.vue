561ada4fc10499b9fe1ec94319fcfc968150b66c
unknown <819563056@qq.com>
2019-09-23 22:17:56 +0800

    黑马头条PC-素材列表-数据加载

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,23 +1,29 @@
 <template>
   <el-card>
-      <bread-crumb slot="header">
-          <template slot="title">素材管理</template>
-      </bread-crumb>
-    <el-tabs v-model="activeName" @tab-click="handleClick">
-    <el-tab-pane label="全部素材" name="all">
+    <bread-crumb slot="header">
+      <template slot="title">素材管理</template>
+    </bread-crumb>
+    <el-tabs v-model="activeName" @tab-click="getMaterial">
+      <el-tab-pane label="全部素材" name="all">
+        <!-- 全部素材 -->
         <div class="img_list">
-        <el-card class="img_item" v-for='item in list' :key='item.id'>
-            <img :src="item.url" alt="">
+          <el-card class="img_item" v-for="item in list" :key="item.id">
+            <img :src="item.url" alt />
             <div class="operate">
-                <i class="el-icon-star-on"></i>
-                <i class="el-icon-delete-solid"></i>
+              <i :style="{color:item.is_collected ? 'red':'#000'}" class="el-icon-star-on"></i>
+              <i class="el-icon-delete-solid"></i>
             </div>
-        </el-card>
+          </el-card>
         </div>
-
-    </el-tab-pane>
-    <el-tab-pane label="收藏素材" name="collect"></el-tab-pane>
-  </el-tabs>
+      </el-tab-pane>
+      <el-tab-pane  label="收藏素材" name="collect">
+        <div class="img_list">
+          <el-card class="img_item" v-for="item in list" :key="item.id">
+            <img :src="item.url" alt />
+          </el-card>
+        </div>
+      </el-tab-pane>
+    </el-tabs>
   </el-card>
 </template>
 
@@ -33,7 +39,7 @@ export default {
     getMaterial () {
       this.$axios({
         url: 'user/images',
-        params: { collect: false }
+        params: { collect: this.activeName === 'collect' }
       }).then(result => {
         this.list = result.data.results
       })
@@ -46,35 +52,34 @@ export default {
 </script>
 
 <style lang='less' scoped>
-.img_list{
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    .img_item{
-        width:180px;
-        height:180px;
-        border-radius: 6px;
-        margin: 30px;
-        position: relative;
-        img{
-            width: 100%;
-            height: 100%;
-        }
-        .operate{
-            background-color:#fff;
-            position: absolute;
-            left: 0;
-            bottom: 0;
-            width: 100%;
-            height: 30px;
-            display: flex;
-            justify-content: space-around;
-            align-items: center;
-            i{
-                font-size: 20px;
-            }
-        }
+.img_list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  .img_item {
+    width: 180px;
+    height: 180px;
+    border-radius: 6px;
+    margin: 30px;
+    position: relative;
+    img {
+      width: 100%;
+      height: 100%;
     }
-
+    .operate {
+      background-color: #fff;
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      width: 100%;
+      height: 30px;
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      i {
+        font-size: 20px;
+      }
+    }
+  }
 }
 </style>
