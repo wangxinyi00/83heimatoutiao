b9dfacca97f6dc2ee052676a6ef8964f926aee32
unknown <819563056@qq.com>
2019-09-28 15:15:31 +0800

    黑马头条PC-页面进度条引入应用

--- src/permission.js
+++ src/permission.js
@@ -1,6 +1,10 @@
 import router from './router'
+import nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
 // 全局设置导航守卫
 router.beforeEach(function (to, from, next) {
+  // 开启进度条
+  nprogress.start()
   // 判断拦截范围
   if (to.path.startsWith('/home')) {
     // 判断是否进入 home 判断有无token
@@ -13,5 +17,10 @@ router.beforeEach(function (to, from, next) {
   } else {
     next()
   }
+})
+// 全局后置守卫
+router.afterEach(function () {
+  // 关闭进度条
+  nprogress.done()
 })
 export default router
